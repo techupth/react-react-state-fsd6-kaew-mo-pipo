17fbbd349a4e5a2ebefd112d7410bea06e1121fd
Kanokphit <kanokphit55@gmail.com>
2024-06-05 11:36:38 +0700

    "React React State Assignment"

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [language, uselanguage] = useState("Greeting Message");
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{language}</div>
       <div className="buttons">
-        <button>สวัสดี!</button>
-        <button>Hi!</button>
-        <button>你好!</button>
+        <button onClick={() => uselanguage("สวัสดี!")}>สวัสดี!</button>
+        <button onClick={() => uselanguage("Hi!")}>Hi!</button>
+        <button onClick={() => uselanguage("你好!")}>你好!</button>
       </div>
     </div>
   );
